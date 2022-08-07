c4581f699bccbc33c6523e586eff1e4f81f0d9bc
CongNT <congnt@investidea.tech>
2022-08-07 17:13:58 +0700

    zxc

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Login from "./pages/Login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./pages/Dashboard"
+import Setup from "./pages/Setup";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Route exact path="/setup" component={Setup} />
         <Route exact path="/login" component={Login} />
-		<Route exact path="/" component={Dashboard} />
+		    <Route exact path="/" component={Dashboard} />
       </BrowserRouter>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,79 +4,124 @@ import {
   passwordValidator,
 } from "../components/regexValidator";
 import { useHistory } from "react-router-dom";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-// import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-// import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import axios from 'axios';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import axios from "axios";
 const Login = () => {
   const history = useHistory();
 
-  const [input, setInput] = React.useState({ name: "", message: "", link: "" });
-  const [date, setDate]	= React.useState(new Date("2020-06-13T00:00:00"));
+  const [input, setInput] = React.useState({
+    nuber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [step, setStep] = React.useState(1);
+  const [user, setUser] = React.useState({});
   const [errorMessage, seterrorMessage] = React.useState("");
   const [successMessage, setsuccessMessage] = React.useState("");
-
+  const [loading, setLoading] = React.useState(false);
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
-  };
-
-  const handleChangeDate = (e) => {
-    setDate(e);
   };
 
   React.useEffect(() => {
     if (localStorage.getItem("auth")) history.push("/");
   }, []);
 
+  const formSubmitterStep1 = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    await axios
+      .get("https://api.secretspage.com/api/user?number=" + input.number)
+      .then((data) => {
+        if (data.data.firstLogin) {
+          setStep(3);
+          setLoading(false);
+          seterrorMessage("");
+          setsuccessMessage(
+            `Cảm ơn ${data.data.number} đã mua hàng, hãy tạo mật khẩu mới cho lần đăng nhập tiếp theo`
+          );
+        } else {
+          setStep(2);
+          setLoading(false);
+          seterrorMessage("");
+          setsuccessMessage("Vui lòng nhập mật khẩu");
+        }
+      })
+      .catch(function (error) {
+        setLoading(false);
+        seterrorMessage("Số điện thoại mua hàng không tồn tại");
+      });
+  };
+
+  const formSubmitterStep2 = (e) => {
+    e.preventDefault();
+	if(input.password.length <6){
+		seterrorMessage("Hãy nhập mật khẩu");
+		return;
+	}
+	setLoading(true);
+	axios.post("https://api.secretspage.com/api/user/login", {
+		number: input.number,
+		password: input.password,
+	}).then(function (res) {
+		setLoading(false);
+		history.push("/setup");
+	}).catch(function (res) {
+		setLoading(false);
+		seterrorMessage("Mật khẩu không đúng!");
+	})
+  };
+
+  const formSubmitterStep3 = (e) => {
+    e.preventDefault();
+	if(input.password.length <6){
+		seterrorMessage("Mật khẩu phải có ít nhất 6 ký tự");
+		return;
+	}
+    if (input.password !== input.confirmPassword) {
+      seterrorMessage("Mật khẩu không khớp");
+      return;
+    } else {
+      setLoading(true);
+      axios
+        .post("https://api.secretspage.com/api/user/update-password", {
+          number: input.number,
+          password: input.password,
+          firstLogin: false,
+        })
+        .then((data) => {
+          setLoading(false);
+          setsuccessMessage("Tạo tài khoản thành công");
+          setTimeout(() => {
+            history.push({
+				pathname: '/setup',
+				state: data.data
+			});
+          }, 100);
+        })
+        .catch(function (error) {
+          setLoading(false);
+          seterrorMessage("Vui lòng tạo lại mật khẩu");
+        });
+    }
+  };
   const formSubmitter = (e) => {
     e.preventDefault();
     setsuccessMessage("");
-    if (!input.name)
-      return seterrorMessage("Please enter name");
-	if (!input.message)
-	  return seterrorMessage("Please enter message");
-	if (!input.link)
-	  return seterrorMessage("Please enter url link");
-	  const user = {
-		name: input.name,
-		message: input.message,
-		timeStart: date,
-		link: input.link
-	  };
-	  try{
-	  let header ={
-		'Access-Control-Allow-Origin': '*'
-	
-	  
+    if (!emailValidator(input.email))
+      return seterrorMessage("Please enter valid email id");
 
-	  }
-	  axios.post(`https://api.secretspage.com/api/user/create`, { user }, {
-		headers: header
-	  })
-		.then(res => {
-
-			window.location.href = "https://www.secretspage.com?link=" + input.link;
-		})
-
-	}catch(err){
-		return seterrorMessage(err)
-		
-	}
-    // if (!passwordValidator(input.password))
-    //   return seterrorMessage(
-    //     "Password should have minimum 8 character with the combination of uppercase, lowercase, numbers and specialcharaters"
-    //   );
+    if (!passwordValidator(input.password))
+      return seterrorMessage(
+        "Password should have minimum 8 character with the combination of uppercase, lowercase, numbers and specialcharaters"
+      );
     // setsuccessMessage('Successfully Validated');
-    // if (input.email !== "admin@a.com" || input.password !== "Password@1")
-    //   return seterrorMessage("Invalid email or password");
+    if (input.email !== "admin@a.com" || input.password !== "Password@1")
+      return seterrorMessage("Invalid email or password");
 
-    // history.push("/");
-    // localStorage.setItem("auth", true);
+    history.push("/");
+    localStorage.setItem("auth", true);
   };
 
   return (
@@ -87,113 +132,200 @@ const Login = () => {
           style={{ backgroundImage: 'url("images/bg-01.jpg")' }}
         >
           <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-            <form
-              className="login100-form validate-form"
-              onSubmit={formSubmitter}
-            >
-              <span className="login100-form-title p-b-49">Setup</span>
-              {errorMessage.length > 0 && (
-                <div style={{ marginBottom: "10px", color: "red" }}>
-                  {errorMessage}
+            {loading ? (
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <CircularProgress />
+              </Box>
+            ) : step === 1 ? (
+              <form
+                className="login100-form validate-form"
+                onSubmit={formSubmitterStep1}
+              >
+                <span className="login100-form-title p-b-49">Login</span>
+                {errorMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "red" }}>
+                    {errorMessage}
+                  </div>
+                )}
+                {successMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "green" }}>
+                    {successMessage}
+                  </div>
+                )}
+                <div
+                  className="wrap-input100 validate-input m-b-23"
+                  data-validate="email is required"
+                >
+                  <span className="label-input100">
+                    Số điện thoại mua hàng:
+                  </span>
+                  <input
+                    className="input100"
+                    type="text"
+                    name="number"
+                    placeholder="0123456789"
+                    onChange={handleChange}
+                  />
+                  <span className="focus-input100" data-symbol="" />
                 </div>
-              )}
-              {successMessage.length > 0 && (
-                <div style={{ marginBottom: "10px", color: "green" }}>
-                  {successMessage}
+                <div className="container-login100-form-btn">
+                  <div className="wrap-login100-form-btn">
+                    <div className="login100-form-bgbtn" />
+                    <button className="login100-form-btn">Xác nhận</button>
+                  </div>
                 </div>
-              )}
-              <div
-                className="wrap-input100 validate-input m-b-23"
-                data-validate="nhập tên đi má"
-              >
-                <span className="label-input100">Name</span>
-                <input
-                  className="input100"
-                  type="text"
-				  name="name"
-                  placeholder="Vd: em iuu"
-                  onChange={handleChange}
-                />
-                <span className="focus-input100" data-symbol="" />
-              </div>
-              <div
-                className="wrap-input100 validate-input"
-                data-validate="Có gì muốn nhắn gửi không"
-              >
-                <span className="label-input100">Message</span>
-                <textarea
-					rows="4"
-                  className="input100 text-area-input"
-                  name="message"
-                  placeholder="Vd: anh yeu em"
-                  onChange={handleChange}
-                />
-                <span className="focus-input100" data-symbol="" />
-              </div>
-              <div
-                className="wrap-input100 validate-input"
-				
-              >
-	
-				<br />
-                <LocalizationProvider dateAdapter={AdapterDateFns} className="input-date">
-                  <DateTimePicker
-				  	className="input-date"
-                    label="Time start loving"
-                    value={input.timeStart}
-                    onChange={handleChangeDate}
-					name="timeStart"
-                    renderInput={(params) => <TextField {...params} />}
-                  />{" "}
-                </LocalizationProvider>
-
-              </div>
-			  <br />
-			  <div
-                className="wrap-input100 validate-input m-b-23"
-                data-validate="Nhập url"
-              >
-		
-                <span style={{ marginTop: "20px" }} className="label-input100 ">URL Link(secretspage.com/link)</span>
-                <input
-                  className="input100"
-                  type="text"
-				  name="link"
-                  placeholder="Vd: emiu  ---> secretspage.com/emiu"
-                  onChange={handleChange}
-                />
-                <span className="focus-input100" data-symbol="" />
-              </div>
-              <div className="text-right p-t-8 p-b-31">
-                <a href="#">...</a>
-              </div>
-              <div className="container-login100-form-btn">
-                <div className="wrap-login100-form-btn">
-                  <div className="login100-form-bgbtn" />
-                  <button className="login100-form-btn">Xác nhận</button>
+                <div className="txt1 text-center p-t-54 p-b-20">
+                  <span>Support</span>
                 </div>
-              </div>
-              <div className="txt1 text-center p-t-54 p-b-20">
-                <span>Donate</span>
-              </div>
-              <div className="flex-c-m">
-                <a href="#" className="login100-social-item bg1">
+                <div className="flex-c-m">
+                <a href="https://www.facebook.com/ntcongg/" className="login100-social-item bg1">
                   <i className="fa fa-facebook" />
                 </a>
-                <a href="#" className="login100-social-item bg2">
-                  <i className="fa fa-twitter" />
+                <a href="https://www.instagram.com/_augustine.ng/" className="login100-social-item bg2">
+                  <i className="fa fa-instagram" />
                 </a>
                 <a href="#" className="login100-social-item bg3">
                   <i className="fa fa-google" />
                 </a>
               </div>
-              <div className="flex-col-c p-t-155">
-                {/* <span className="txt1 p-b-17">Or Sign Up Using</span>
+                {/* <div className="flex-col-c p-t-155">
+                <span className="txt1 p-b-17">Or Sign Up Using</span>
                 <a href="#" className="txt2">
                   Sign Up
-                </a> */}
+                </a>
+              </div> */}
+              </form>
+            ) : step === 2 ? (
+              <form
+                className="login100-form validate-form"
+                onSubmit={formSubmitterStep2}
+              >
+                <span className="login100-form-title p-b-49">Login</span>
+                {errorMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "red" }}>
+                    {errorMessage}
+                  </div>
+                )}
+                {successMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "green" }}>
+                    {successMessage}
+                  </div>
+                )}
+                <div
+                  className="wrap-input100 validate-input"
+                  data-validate="Password is required"
+                >
+                  <span className="label-input100">Password</span>
+                  <input
+                    className="input100"
+                    type="password"
+                    name="password"
+                    placeholder="Type your password"
+                    onChange={handleChange}
+                  />
+                  <span className="focus-input100" data-symbol="" />
+                </div>
+                <div className="text-right p-t-8 p-b-31">
+                  <a href="#">Forgot password?</a>
+                </div>
+                <div className="container-login100-form-btn">
+                  <div className="wrap-login100-form-btn">
+                    <div className="login100-form-bgbtn" />
+                    <button className="login100-form-btn">Xác nhận</button>
+                  </div>
+                </div>
+                <div className="txt1 text-center p-t-54 p-b-20">
+                  <span>Or Sign Up Using</span>
+                </div>
+                <div className="flex-c-m">
+                <a href="https://www.facebook.com/ntcongg/" className="login100-social-item bg1">
+                  <i className="fa fa-facebook" />
+                </a>
+                <a href="https://www.instagram.com/_augustine.ng/" className="login100-social-item bg2">
+                  <i className="fa fa-instagram" />
+                </a>
+                <a href="#" className="login100-social-item bg3">
+                  <i className="fa fa-google" />
+                </a>
               </div>
-            </form>
+                {/* <div className="flex-col-c p-t-155">
+                <span className="txt1 p-b-17">Or Sign Up Using</span>
+                <a href="#" className="txt2">
+                  Sign Up
+                </a>
+              </div> */}
+              </form>
+            ) : (
+              <form
+                className="login100-form validate-form"
+                onSubmit={formSubmitterStep3}
+              >
+                <span className="login100-form-title p-b-49">Login</span>
+                {errorMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "red" }}>
+                    {errorMessage}
+                  </div>
+                )}
+                {successMessage.length > 0 && (
+                  <div style={{ marginBottom: "10px", color: "green" }}>
+                    {successMessage}
+                  </div>
+                )}
+                <div
+                  className="wrap-input100 validate-input m-b-23"
+                  data-validate="Password is required"
+                >
+                  <span className="label-input100">Password</span>
+                  <input
+                    className="input100"
+                    type="password"
+                    name="password"
+                    placeholder="Type your password"
+                    onChange={handleChange}
+                  />
+                  <span className="focus-input100" data-symbol="" />
+                </div>
+
+                <div
+                  className="wrap-input100 validate-input"
+                  data-validate="Password is required"
+                >
+                  <span className="label-input100">Confirm Password</span>
+                  <input
+                    className="input100"
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm your password"
+                    onChange={handleChange}
+                  />
+                  <span className="focus-input100" data-symbol="" />
+                </div>
+
+                <div className="text-right p-t-8 p-b-31">
+                  {/* <a href="#">Forgot password?</a> */}
+                </div>
+                <div className="container-login100-form-btn">
+                  <div className="wrap-login100-form-btn">
+                    <div className="login100-form-bgbtn" />
+                    <button className="login100-form-btn">Xác nhận</button>
+                  </div>
+                </div>
+                <div className="txt1 text-center p-t-54 p-b-20">
+                  <span>Support</span>
+                </div>
+                <div className="flex-c-m">
+                <a href="https://www.facebook.com/ntcongg/" className="login100-social-item bg1">
+                  <i className="fa fa-facebook" />
+                </a>
+                <a href="https://www.instagram.com/_augustine.ng/" className="login100-social-item bg2">
+                  <i className="fa fa-instagram" />
+                </a>
+                <a href="#" className="login100-social-item bg3">
+                  <i className="fa fa-google" />
+                </a>
+              </div>
+              </form>
+            )}
           </div>
         </div>
       </div>
